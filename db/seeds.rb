ff3da5a84f37dca6d4cbe1a8c38f35f4bd277838
Andre Menezes <djdedemenezes@gmail.com>
2021-05-26 14:10:57 -0300

    Seeded database with 5 FAKER restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+PHONE = 
+
+puts "Generating DB"
+
+5.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name ,
+    address: Faker::Space.distance_measurement + " Street",
+    phone_number: Faker::PhoneNumber.subscriber_number(length: 9),
+    category: Restaurant::CATEGORY.sample
+  )
+  puts restaurant.name
+end
+
+puts "DB generated"
