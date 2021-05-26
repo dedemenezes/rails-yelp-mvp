9b6e4ad48b00e57612368cdd515e8e9504ea388f
Andre Menezes <djdedemenezes@gmail.com>
2021-05-26 14:03:24 -0300

    Models created with validations and table connections

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,12 @@
 class Restaurant < ApplicationRecord
+
+  CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
+  
+  has_many :reviews, dependent: :destroy
+
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: Restaurant::CATEGORY,
+    message: "%{value} is not a valid option. Chosse between:  %w(chinese italian japanese french belgian)" }
+
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,10 @@
 class Review < ApplicationRecord
+
+  RATING = (0..5).to_a
+  
+  belongs_to :restaurant
+
+  validates :content, presence: true
+  validates :rating, inclusion: { in: RATING, message: "%{value} needs to be between 1 and 5"}, numericality: { only_integer: true }
+
 end
