51b1ea9e526dec745035ed5bf8569d428503fec0
Andre Menezes <djdedemenezes@gmail.com>
2021-05-26 14:30:32 -0300

    restaurants#index done. no CSS

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,29 @@
 class RestaurantsController < ApplicationController
+
+  before_action :set_restaurant, only: [:show]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  
+  end
+  
+
+  def new
+  end
+
+  def create
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = @restaurants.find(params[:id])    
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
